b3f8954020cbc2aa65c225b4a052a23835a5f1ca
hcmos <c1000702@planet.kanazawa-it.ac.jp>
2024-12-22 14:20:29 +0900

    実験

--- src/ball_detector.cpp
+++ src/ball_detector.cpp
@@ -52,8 +52,9 @@ namespace ball_detector
 
   void BallDetector::pointcloud_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
   {
-    // if (!is_autonomous)
-    //   return;
+    if (!is_autonomous)
+       return;
+    auto start = std::chrono::high_resolution_clock::now();
     rclcpp::Time current_time = this->now();
     double dt = (previous_time_.nanoseconds() == 0) ? 0.0 : (current_time - previous_time_).seconds();
     previous_time_ = current_time;
@@ -73,6 +74,7 @@ namespace ball_detector
 
     // マーカーのパブリッシュ
     publish_markers(ball_position, clusters, remaining_cloud);
+    RCLCPP_INFO(this->get_logger(), "exec time: %ld ms", std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count());
   }
 
   std::vector<Point3D> BallDetector::preprocess_pointcloud(const sensor_msgs::msg::PointCloud2 &msg)
